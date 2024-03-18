967b7333441e987bf127e570c8fb3f142fd68f85
Carolyne Ndunge <carohndunge@gmail.com>
2024-03-19 00:15:33 +0300

    First commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,18 @@
 import React from "react";
+//import "./index.css";
 import ProfileCard from "./profile-card";
+import AlexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
 
 function App() {
-  const [image, setImage] = React.useState(null);
-  const [image1, setImage1] = React.useState(null);
-  const [image2, setImage2] = React.useState(null);
-
-  const fetchImage = (img) => {
-    setImage(img);
-  };
-  const fetchImage1 = (img) => {
-    setImage1(img);
-  };
-  const fetchImage2 = (img) => {
-    setImage2(img);
-  };
-
   return (
     <div>
       <div>Personal Card Assistant</div>
 
-      <ProfileCard
-        title="Alexa"
-        handle="@alexa99"
-        item="a"
-        fetchImageProp={fetchImage}
-        image={image}
-      />
-      <ProfileCard
-        title="Cortana"
-        handle="@cortana32"
-        item="c"
-        fetchImageProp={fetchImage1}
-        image={image1}
-      />
-      <ProfileCard
-        title="Siri"
-        handle="@siri01"
-        item="s"
-        fetchImageProp={fetchImage2}
-        image={image2}
-      />
+      <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
+      <ProfileCard title="Cortana" handle="@cortana32" image={CortanaImage} />
+      <ProfileCard title="Siri" handle="@siri01" image={SiriImage} />
     </div>
   );
 }
--- src/profile-card.jsx
+++ src/profile-card.jsx
@@ -1,29 +1,18 @@
-import React from "react";
-import AlexaImage from "./images/alexa.png";
-import CortanaImage from "./images/cortana.png";
-import SiriImage from "./images/siri.png";
+import React, { useState } from "react";
 
-function ProfileCard({ title, handle, item, image, fetchImageProp }) {
-  const [img, setImage] = React.useState(null);
-  const [img1, setImage1] = React.useState(null);
-  const [img2, setImage2] = React.useState(null);
+function ProfileCard({ title, handle, image }) {
+  const [showImage, setShowImage] = useState(false);
 
   const handleClick = () => {
-    if (item === "a") {
-      setImage(AlexaImage);
-      fetchImageProp(img);
-    } else if (item === "c") {
-      setImage1(CortanaImage);
-      fetchImageProp(img1);
-    } else if (item === "s") {
-      setImage2(SiriImage);
-      fetchImageProp(img);
-    }
+    setShowImage(true);
   };
+
   return (
     <div>
-      <img src={image} alt="pda logo" />
-      <button onClick={handleClick}>Click to view image</button>
+      <div>
+        <button onClick={handleClick}>Click to view image</button>
+      </div>
+      {showImage && <img src={image} alt="Image" />}
       <div>Title is {title}</div>
       <div>Handle is {handle}</div>
     </div>
